091d3511904bf27749604676ac15be1902cf289d
Jay <huny000315@gmail.com>
2025-12-08 16:03:25 +0900

    Initial project setup with MySQL and Docker

---
+++ src/main/java/com/mountain/for_mountain/ForMountainApplication.java
@@ -0,0 +1,13 @@
+package com.mountain.for_mountain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForMountainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForMountainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mountain/for_mountain/ForMountainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountain.for_mountain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForMountainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
